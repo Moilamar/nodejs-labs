f3eb0a28959dc211a286f11114c91fbf75068fc5
Markus Moilanen <markus.moilanen@protacon.com>
2020-01-03 13:34:16 +0200

    add postgress db and query test data

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,28 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const pg = require("pg");
+
+const connectionString = "postgres://postgres:postgres@localhost:5432/express_psql";
+const client = new pg.Client(connectionString);
+client.connect();
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log("index");
-  res.status(200).render('index', { title: 'Nodejs-labs', testData: ["first", "second"] });
+router.get("/", function(req, res, next) {
+  const query = client.query("SELECT * FROM test_data LIMIT 30;");
+  query.then((result) => {
+    //console.log(result);
+  });
+
+  const rows = [];
+  query.on("row", (result) => {
+    console.log(result);
+    rows.push(result);
+  });
+
+  query.on("end", () => {
+    res.status(200).render("index", { title: "Nodejs-labs", testData: rows });
+  });
 });
 
 module.exports = router;
